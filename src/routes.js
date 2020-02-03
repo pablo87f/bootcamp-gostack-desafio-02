3ae81403f1f072c1373a554afa3a274344cda998
Pablo Fernandes <pablofern87@gmail.com>
2020-02-03 20:46:27 -0300

    feat: - Criando arquivos app, server e routes - Configurando nodemon e sucrase pra executarem em debug

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+    const data = { message: 'ok' }
+    return res.json(data)
+});
+
+export default routes;
