bf0eb323143e54df002277d5833256f071daf8f0
Pablo Fernandes <pablofern87@gmail.com>
2020-02-04 20:05:58 -0300

    feat: - Configurando o prettier, eslint e editorconfig

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,8 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    const data = { message: 'ok' }
-    return res.json(data)
+    const data = { message: 'ok' };
+    return res.json(data);
 });
 
 export default routes;
